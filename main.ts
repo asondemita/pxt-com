d2c16cca91937acc5e7842375ed19c9c1948fef4
asondemita <asondemita@gmail.com>
2018-09-09 04:10:49 +0900

    test

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . # . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\u30A2" color=#EC7505 block="コメント"
+namespace comment {
+    //% blockId=show_strings block="コメント %v"
+    export function noaction(): void {
+    }
+}
