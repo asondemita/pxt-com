b5e5f64d2d51a7351f00a1d1b09c31d2a771564e
asondemita <asondemita@gmail.com>
2018-09-09 06:40:39 +0900

    test4

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=70 icon="\u30A2" color=#EC7505 block="コメント"
+//% weight=70 icon="" color=#888888 block="コメント"
 namespace comment {
     //% blockId=show_strings block="コメント %v"
     export function noaction(text: string): void {
